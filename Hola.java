c2ce50bde6b0602b343fd7e8c73be50f5a81dcd9
Serafim1331 <serafim13serafim31@ukr.net>
2023-06-03 14:48:31 +0300

    Third commit

---
+++ Hola.java
@@ -0,0 +1,5 @@
+public class Hola {
+    public void Hola(){
+        System.out.println("Hola, mundo!");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Hola hola = new Hola();
+        hola.Hola();
 
         System.out.println("Hello world!");
+
     }
 }
